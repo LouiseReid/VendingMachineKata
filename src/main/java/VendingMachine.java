530cf5cda3c6c8e733d03a407998dd454349c308
Louise Reid <l_reid@hotmail.co.uk>
2018-03-05 14:45:31 +0000

    add money and get refund in vendingmachine class

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -1,2 +1,31 @@
+import java.util.HashMap;
+
 public class VendingMachine {
+    private HashMap<Selection, Tray> products;
+    private double cash;
+    private double credit;
+
+    public VendingMachine(HashMap<Selection, Tray> products, double cash) {
+        this.products = products;
+        this.cash = cash;
+        this.credit = 0.00;
+    }
+
+    public double getCash() {
+        return cash;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void addMoney(double amountEntered){
+        credit += amountEntered;
+    }
+
+    public double getRefund(){
+        double refundAmount = credit;
+        credit = 0;
+        return refundAmount;
+    }
 }
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,6 +1,9 @@
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
 
 public class VendingMachineTest {
 
@@ -20,6 +23,23 @@ public class VendingMachineTest {
             }
             products.put(selection, tray);
         }
-        vendingMachine = new VeningMachine(products, 10.00);
+        vendingMachine = new VendingMachine(products, 10.00);
+    }
+
+    @Test
+    public void testCoinsAddToCredit(){
+        vendingMachine.addMoney(0.50);
+        assertEquals(0.50, vendingMachine.getCredit(), 0.01);
+        assertEquals(10.00, vendingMachine.getCash(), 0.01);
+    }
+
+    @Test
+    public void testCanGiveRefund(){
+        vendingMachine.addMoney(1.50);
+        double refunded = vendingMachine.getRefund();
+        assertEquals(0.00, vendingMachine.getCredit(), 0.01);
+        assertEquals(1.50, refunded, 0.01);
+        assertEquals(10.00, vendingMachine.getCash(), 0.01);
+
     }
 }
